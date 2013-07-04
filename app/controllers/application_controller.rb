da6d226466368f3369cd3830577df82d0fc0103c
cdeliens <cdeliens@gmail.com>
2013-07-04 15:33:22 -0600

    handling callback on pages_controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,27 +2,6 @@ class ApplicationController < ActionController::Base
   protect_from_forgery
   respond_to :json
 
-  def index
-    index! do |format|
-      if params[:callback]
-        format.json { render :json => collection.to_json, :include => :category, :callback => params[:callback] }
-      else
-        format.json { render json: collection.to_json}
-       end
-    end
-    
-  end
-
-  def show
-    show! do |format|
-      if params[:callback]
-        format.json { render :json => resource.to_json, :include => :category, :callback => params[:callback] }
-      else
-        format.json { render json: resource.to_json}
-       end
-    end
-    
-  end
 
   def require_http_auth_on_staging
     if Rails.env == "production" && !request.headers['HTTP_USER_AGENT'].eql?('facebookexternalhit/1.1 (+http://www.facebook.com/externalhit_uatext.php)')
--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,3 +1,23 @@
 class PagesController < InheritedResources::Base
+  def index
+    index! do |format|
+      if params[:callback]
+        format.json { render :json => collection.to_json, :include => :category, :callback => params[:callback] }
+      else
+        format.json { render json: collection.to_json}
+       end
+    end
+    
+  end
 
+  def show
+    show! do |format|
+      if params[:callback]
+        format.json { render :json => resource.to_json, :include => :category, :callback => params[:callback] }
+      else
+        format.json { render json: resource.to_json}
+       end
+    end
+    
+  end
 end
